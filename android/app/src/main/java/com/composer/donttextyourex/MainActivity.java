1c57e1b988861e499d934b92c9c28a66c5338e23
composerr <34034600+composerr@users.noreply.github.com>
2026-07-18 09:14:42 +0000

    feat: integrate Capacitor and update build workflow

---
+++ android/app/src/main/java/com/composer/donttextyourex/MainActivity.java
@@ -0,0 +1,5 @@
+package com.composer.donttextyourex;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.composer.donttextyourex',
+appName: "Don't Text Your Ex",
+  webDir: 'dist'
+};
+
+export default config;
